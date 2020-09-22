66b2183d6d3cd06456c21a30f09fe896b6e08655
SherdenZ <zuhio76@gmail.com>
2020-09-22 19:11:08 +0400

    fix: fix little bug

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -3,7 +3,7 @@ const CustomError = require("../extensions/custom-error");
 const MODERN_ACTIVITY= 15; 
 const HALF_LIFE_PERIOD= 5730;
 
-module.exports = function dateSample(/sampleActivity) {
+module.exports = function dateSample(sampleActivity) {
   throw new CustomError('Not implemented');
   const RATE_OF_REACTION = 0.693 / HALF_LIFE_PERIOD;
   
